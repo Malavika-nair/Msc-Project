03267fb747e80b0b761f061189e7c8af55757633
mrn12 <mrn12@183b2221-cc75-48b0-944a-a1ce4233b9e8>
2015-01-16 21:16:29 +0000

    Solution using C# for image streaming

---
+++ code/tags/BackendApp/Models/Image.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BackendApp.Models
+{
+   //The properties of the resources are added to the model class
+    public class Image
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+    
+    public class ImageCount
+    {
+        public int Number { get; set; }
+    }
+}
---
+++ code/tags/BackendApp/Scripts/StreamImage.js
@@ -0,0 +1,85 @@
+﻿$(document).ready(function () {
+    var count = 0;
+    $('#start').attr("disabled", false);
+    $('#stop').attr("disabled", true);
+
+  
+   
+    function calculateCount(callback) {
+        var uri1 = 'api/images';
+
+        //sending ajax request
+        $.getJSON(uri1)
+         .done(function (data) {
+             callback(data[0].Number);
+             
+         });
+        
+    }
+   
+    $('#start').on('click', function () {
+        $('#start').attr("disabled", true);
+        $('#stop').attr("disabled", false);
+        var intervalId = 200;
+        var index = 1;
+        intervalId = setInterval(function () {
+
+            calculateCount(function (value) {
+           count = value;
+        
+       });
+
+         
+          
+           $.ajax({
+               type: 'GET',
+               data: JSON,
+               url: 'api/images/' + index,
+               cache: false,
+               success: function (images) {
+
+                   $.each(images, function (i, image) {
+                       console.log(image.Path);
+
+                       $("img").attr("src", image.Path);
+                      
+
+                   });
+                
+                   if (index < count) {
+                       index++;
+                   }
+                   else {
+                       index = count;
+
+                   }
+
+
+               }
+
+
+           });
+    
+
+          
+
+            $('#stop').on('click', function () {
+                $('#start').attr("disabled", false);
+                $('#stop').attr("disabled", true);
+                clearInterval(intervalId);
+                index = 1;
+                count = 0;
+            });
+
+
+        }, 200);
+
+
+        $('#start').attr("disabled", false);
+
+
+
+    });
+
+
+});
